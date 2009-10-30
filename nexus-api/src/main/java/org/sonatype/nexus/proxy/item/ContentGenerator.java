aa5705d30e3f247645bce852bf916436f804a41d
cstamas <cstamas@2aa8b3fc-8ebb-4439-a84f-95066eaea8ab>
2009-10-30 14:01:16 +0000

    NEXUS-2936: added javadoc describing how a generator should behave
    git-svn-id: file:///opt/svn/repositories/sonatype.org/nexus/branches/nexus-2936@5443 2aa8b3fc-8ebb-4439-a84f-95066eaea8ab

--- nexus-api/src/main/java/org/sonatype/nexus/proxy/item/ContentGenerator.java
+++ nexus-api/src/main/java/org/sonatype/nexus/proxy/item/ContentGenerator.java
@@ -10,7 +10,10 @@ import org.sonatype.plugin.ExtensionPoint;
 
 /**
  * A content generator is a special component, that is able to generate content on-the-fly, that will be substituted
- * with the content coming from the Local Storage.
+ * with the content coming from the Local Storage. The size if handled automatically (ContentLocator carries it),
+ * but the developer should choose how to handle the item's creation date! Important, since Nexus REST API
+ * handles conditional GET's, and the "generated" file will have the _same_ timestamp coming from the LocalRepository
+ * unless explicitly overridden in generateContent() method body with code like item.setModified(long)!
  * 
  * @author cstamas
  */
